b40d69b338e393c50f68bd263abba6962eecbbe7
mnirupan <mosesnirupan@gmail.com>
2025-05-24 17:34:41 +0530

    #2, server initialized in server folder

---
+++ server/src/main/java/com/server/server/HelloController.java
@@ -0,0 +1,12 @@
+package com.server.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Spring Boot!";
+    }
+}
